95f1f8a79cdd4f4c8a33965da387eb74e7ba23e4
shoeb khan <shoebkhan4738@gmail.com>
2024-07-27 18:48:46 +0530

    new project

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-const profile = document.querySelector('.profile_image');
+const inputBox = document.querySelector('#input-box');
+const container = document.querySelector('#list-container');
 
-profile.addEventListener('click',() => {
+function addtast(){
+    if(inputBox.value === ''){
+        alert("You Must Write Something!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        container.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
 
-});
+container.addEventListener("click",function(e) {
+    if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    } 
+    else if(e.target.tagName === "LI"){
+        e.target.classList.toggle("check");
+        saveData();
+    }
+},false);
+
+function saveData() {
+    localStorage.setItem("data", container.innerHTML);
+}
+function showTast() {
+    container.innerHTML = localStorage.getItem("data");
+}
+showTast();
